3b955cd32e35fdba75c5f3c75e784022a05aa3cd
wming <wming217@outlook.com>
2024-01-02 21:47:39 +0800

    feat: add

--- app/api/geminiAIChat/route.ts
+++ app/api/geminiAIChat/route.ts
@@ -21,9 +21,9 @@ export const preferredRegion = [
 ]
 
 const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  // "Access-Control-Allow-Origin": "*",
+  // "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  // "Access-Control-Allow-Headers": "Content-Type, Authorization",
 }
 
 export async function POST(request: Request) {
