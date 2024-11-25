4eb77724ee38805233556759b0e3bd778b4ff952
wming <btming217@gmail.com>
2024-11-25 21:53:09 +0800

    feat: fix

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import { google } from "@ai-sdk/google";
 import { StreamData, streamText } from "ai";
-import "dotenv";
+import "dotenv/config";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { stream } from "hono/streaming";
@@ -8,12 +8,16 @@ import { handle } from "hono/vercel";
 import { Message } from "../types";
 
 export const config = {
-  runtime: "edge",
+  // runtime: "edge",
   regions: ["cle1", "iad1", "pdx1", "sfo1", "sin1", "syd1", "hnd1", "kix1"],
 };
 
 const app = new Hono().basePath("/api");
 app.use("*", cors({ origin: "*", allowHeaders: ["Content-Type"] }));
+
+app.get("/", (c) => {
+  return c.json({ message: "Congrats! You've deployed Hono to Vercel" });
+});
 
 app.post("/geminiChat", async (c) => {
   const data = new StreamData();
@@ -30,36 +34,15 @@ app.post("/geminiChat", async (c) => {
 
   return stream(c, (stream) => stream.pipe(result.toDataStream({ data })));
 });
-app.get("/", async (c) => {
+
+app.get("/verison", async (c) => {
   return c.json({
     data: "ok",
   });
 });
+const handler = handle(app);
 
-// app.post("/geminiChatWithImage", async (c) => {
-//   const { prompt, imageBase, ...rest } = (await c.req.json()) as Omit<
-//     BodyData,
-//     "history"
-//   >;
-
-// const result = streamText({
-//   model: google("gemini-1.5-flash"),
-//   // ...rest,
-//   // [prompt,imageBase]
-// });
-// const model = genAI.getGenerativeModel({
-//   model: "gemini-pro-vision",
-//   ...rest,
-// });
-
-// try {
-//   const res = await model.generateContentStream([prompt, ...imageBase]);
-//   const stream = GoogleGenerativeAIStream(res);
-//   return new StreamingTextResponse(stream);
-// } catch (error) {
-//   c.status(400);
-//   return c.json({ data: String(error) });
-// }
-// });
-
-export default handle(app);
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
