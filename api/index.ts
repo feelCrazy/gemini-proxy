bf347fffb8a9d66edf64c1bdc31650b9c13dca2e
wming <btming217@gmail.com>
2024-11-25 21:30:06 +0800

    feat: upgrade

--- api/index.ts
+++ api/index.ts
@@ -1,60 +1,60 @@
-import { Hono } from "hono"
-import { handle } from "hono/vercel"
-import { GoogleGenerativeAI } from "@google/generative-ai"
-import { GoogleGenerativeAIStream, StreamingTextResponse } from "ai"
-import { cors } from "hono/cors"
-import { BodyData } from "../types"
-
-const API_KEY = process.env.API_KEY || ""
-const genAI = new GoogleGenerativeAI(API_KEY)
+import { google } from "@ai-sdk/google";
+import { StreamData, streamText } from "ai";
+import "dotenv";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { stream } from "hono/streaming";
+import { handle } from "hono/vercel";
+import { Message } from "../types";
 
 export const config = {
   runtime: "edge",
   regions: ["cle1", "iad1", "pdx1", "sfo1", "sin1", "syd1", "hnd1", "kix1"],
-}
+};
 
-const app = new Hono().basePath("/api")
-app.use("*", cors({ origin: "*", allowHeaders: ["Content-Type"] }))
+const app = new Hono().basePath("/api");
+app.use("*", cors({ origin: "*", allowHeaders: ["Content-Type"] }));
 
 app.post("/geminiChat", async (c) => {
-  const { history, prompt, ...rest } = (await c.req.json()) as Omit<
-    BodyData,
-    "imageBase"
-  >
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
-    ...rest,
-  })
-  const chat = model.startChat({ history })
+  const data = new StreamData();
+  data.append("initialized call");
+  const { messages } = (await c.req.json()) as Message;
+  const result = streamText({
+    model: google("gemini-1.5-flash"),
+    messages,
+    onFinish() {
+      data.append("call completed ");
+      data.close();
+    },
+  });
 
-  try {
-    const res = await chat.sendMessageStream(prompt)
-    const stream = GoogleGenerativeAIStream(res)
-    return new StreamingTextResponse(stream)
-  } catch (error) {
-    c.status(400)
-    return c.json({ data: String(error) })
-  }
-})
+  return stream(c, (stream) => stream.pipe(result.toDataStream({ data })));
+});
 
-app.post("/geminiChatWithImage", async (c) => {
-  const { prompt, imageBase, ...rest } = (await c.req.json()) as Omit<
-    BodyData,
-    "history"
-  >
-  const model = genAI.getGenerativeModel({
-    model: "gemini-pro-vision",
-    ...rest,
-  })
+// app.post("/geminiChatWithImage", async (c) => {
+//   const { prompt, imageBase, ...rest } = (await c.req.json()) as Omit<
+//     BodyData,
+//     "history"
+//   >;
 
-  try {
-    const res = await model.generateContentStream([prompt, ...imageBase])
-    const stream = GoogleGenerativeAIStream(res)
-    return new StreamingTextResponse(stream)
-  } catch (error) {
-    c.status(400)
-    return c.json({ data: String(error) })
-  }
-})
+// const result = streamText({
+//   model: google("gemini-1.5-flash"),
+//   // ...rest,
+//   // [prompt,imageBase]
+// });
+// const model = genAI.getGenerativeModel({
+//   model: "gemini-pro-vision",
+//   ...rest,
+// });
 
-export default handle(app)
+// try {
+//   const res = await model.generateContentStream([prompt, ...imageBase]);
+//   const stream = GoogleGenerativeAIStream(res);
+//   return new StreamingTextResponse(stream);
+// } catch (error) {
+//   c.status(400);
+//   return c.json({ data: String(error) });
+// }
+// });
+
+export default handle(app);
--- types/index.ts
+++ types/index.ts
@@ -1,22 +1,3 @@
-import { SafetySetting } from "@google/generative-ai"
+import type { CoreMessage } from "ai";
 
-export type BodyData = {
-  history?: {
-    role: string
-    parts: string
-  }[]
-  imageBase: ImageBase[]
-
-  prompt: string
-  generationConfig?: {
-    [key: string]: string | number
-  }
-  safetySettings?: SafetySetting[]
-}
-
-type ImageBase = {
-  inlineData: {
-    data: string
-    mimeType: string
-  }
-}
+export type Message = { messages: CoreMessage[] };
